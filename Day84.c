0c92640b41d9a51eea2c67a994ed86adb4a4fff8
Chirag Singh <Chirag.21947@stu.upes.ac.in>
2026-04-25 16:42:53 +0530

    Day84.c

---
+++ Day84.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * insertionSort - Sorts an array of integers using the insertion sort algorithm.
+ * @arr: The array to be sorted
+ * @n: Number of elements in the array
+ */
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are greater than key,
+           to one position ahead of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+
+    // Read the number of elements
+    if (scanf("%d", &n) != 1) return 0;
+
+    int arr[n];
+
+    // Read n space-separated integers
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(arr, n);
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++) {
+        printf("%d%s", arr[i], (i == n - 1) ? "" : " ");
+    }
+    printf("\n");
+
+    return 0;
+}
